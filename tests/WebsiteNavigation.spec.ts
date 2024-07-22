3e14a910ecab8188da3c88fec9f712e9c72d9210
Victor Reyes <100883441+reyesvictor1@users.noreply.github.com>
2024-07-21 22:59:24 -0600

    Added WebsiteNavigation spec file and first tests.

---
+++ tests/WebsiteNavigation.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach( async ({ page }) => {
+    console.log(`Running ${test.info().title}`);
+    await page.goto("https://demo.guru99.com/test/newtours/index.php");
+});
+
+test("website navigation", async ({ page }) => {
+
+    // starting test in home page
+    await expect(page).toHaveTitle("Welcome: Mercury Tours");
+
+    // test SIGN-ON link
+    await page.getByRole("link", {name: "SIGN-ON"}).click();
+    await expect(page).toHaveURL("https://demo.guru99.com/test/newtours/login.php");
+    await expect(page).toHaveTitle("Sign-on: Mercury Tours");
+
+    // test REGISTER link
+    await page.getByRole("link", {name: "REGISTER"}).click();
+    await expect(page).toHaveURL("https://demo.guru99.com/test/newtours/register.php");
+    await expect(page).toHaveTitle("Register: Mercury Tours");
+
+    // test SUPPORT link
+    await page.getByRole("link", {name: "SUPPORT"}).click();
+    await expect(page).toHaveURL("https://demo.guru99.com/test/newtours/support.php");
+    await expect(page).toHaveTitle("Under Construction: Mercury Tours");
+
+    // test CONTACT link
+    await page.getByRole("link", {name: "CONTACT"}).click();
+    await expect(page).toHaveURL("https://demo.guru99.com/test/newtours/support.php");
+    await expect(page).toHaveTitle("Under Construction: Mercury Tours");
+
+});
